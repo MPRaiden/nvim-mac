86f79d5547fe8c7978682244e9313debbd9509b7
Marin Peric Radan <marinpericradan1@gmail.com>
2024-11-22 10:26:25 +0100

    Fix for tmux/term/nvim colors

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -4,22 +4,18 @@ function Color(color)
   vim.api.nvim_set_hl(0, 'Normal', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'StatusLine', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
 
   if color == 'gruvbuddy' then
     vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
     vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
-  end
-
-  if color == 'tokyonight' then
-    vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'StatusLine', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
-    vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
-    vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
   end
 end
 
