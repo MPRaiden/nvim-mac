b20dcbbc298655aaab30f8b96890b7d99962b912
Marin Peric Radan <marinpericradan1@gmail.com>
2024-11-24 12:05:04 +0100

    Colors.lua file cleanup

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -2,6 +2,7 @@ function Color(color)
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, 'Normal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
@@ -10,36 +11,18 @@ function Color(color)
   vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
   vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
   vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
-
-  if color == 'gruvbuddy' then
-    vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
-    vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
-  end
+  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
 end
 
 return {
   {
     'tjdevries/colorbuddy.nvim',
+    enabled = true,
     lazy = false,
     priority = 1000,
-    enabled = true,
     config = function()
       Color 'gruvbuddy'
-    end,
-  },
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    enabled = false,
-    opts = {},
-    config = function()
-      require('tokyonight').setup {
-        style = 'night',
-        transparent = true,
-        terminal_colors = true,
-      }
-      Color 'tokyonight'
     end,
   },
 }
