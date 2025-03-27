1ff329c771f2f9f1a0f7d23df308446df3ae3976
Marin Peric Radan <marinpericradan1gmail.com>
2025-03-27 09:30:46 +0100

    chore: added rose-pine theme to colors.lua

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -35,4 +35,14 @@ return {
       vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
+  {
+    'rose-pine/neovim',
+    enabled = false,
+    lazy = false,
+    priority = 1000,
+    name = 'rose-pine',
+    config = function()
+      vim.cmd.colorscheme 'rose-pine'
+    end,
+  },
 }
