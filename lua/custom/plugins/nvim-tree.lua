dd4a71945b3b79d610d350e8f312a7e21a922992
Marin Peric Radan <marin.peric@seekandhit.com>
2025-06-12 13:58:49 +0200

    chore: updated nvim-tree to show dot & hidden files as well

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -5,7 +5,11 @@ return {
       -- example customization
       view = { side = 'left', width = 40 },
       update_focused_file = { enable = true, update_root = true },
-      -- ...other nvim-tree options
+
+      filters = {
+        dotfiles = false,
+        git_ignored = false,
+      },
 
       -- In normal mode, toggle nvim-tree with Ctrl+n
       vim.keymap.set('n', '<C-n>', '<cmd>:NvimTreeToggle<CR>', { desc = 'Toggle File Explorer' }),
