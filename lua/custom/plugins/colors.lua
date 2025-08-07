eb84505885b24165ea59633df14f6d7614b89755
Marin Peric Radan <marin.peric@seekandhit.com>
2025-08-07 07:15:24 +0200

    colorbuddy theme

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -1,3 +1,20 @@
+function Color(color)
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, 'Normal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'SignColumn', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'CmpItemMenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TelescopeBorder', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Pmenu', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'TroubleNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'WhichKeyNormal', { link = 'Normal' })
+  vim.api.nvim_set_hl(0, 'Whitespace', { fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#4c4c4c' })
+  vim.api.nvim_set_hl(0, 'TelescopeSelection', { bg = '#161a2e' })
+  vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#1790eb', bg = 'none' })
+end
+
 return {
   {
     'rose-pine/neovim',
@@ -35,7 +52,7 @@ return {
   },
   {
     'folke/tokyonight.nvim',
-    enabled = true,
+    enabled = false,
     lazy = false,
     priority = 10000,
     opts = {
@@ -71,6 +88,15 @@ return {
     },
     config = function()
       vim.cmd.colorscheme 'melange'
+    end,
+  },
+  {
+    'tjdevries/colorbuddy.nvim',
+    enabled = true,
+    lazy = false,
+    priority = 10000,
+    config = function()
+      Color 'gruvbuddy'
     end,
   },
 }
